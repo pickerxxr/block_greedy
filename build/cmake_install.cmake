2de33e7522999726fb135f00212d9b648814e1ac
pickerxxr <18307100052@fudan.edu.cn>
2021-07-12 04:49:08 +0000

    whatever

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /root/Desktop/other_paper_work/HEP/hybrid_edge_partitioner
+# Install script for directory: /root/summer/block_greedy/block_greedy
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/root/Desktop/other_paper_work/HEP/hybrid_edge_partitioner/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/root/summer/block_greedy/block_greedy/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
